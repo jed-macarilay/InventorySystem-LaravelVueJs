7f0e88ce6dbf6eedacb0ee8e397d3f6129f591e7
unknown <jed.macarilay@gmail.com>
2022-11-29 06:11:20 +0800

    Added new colum in shipping table

---
+++ database/migrations/2022_11_29_060418_add_extra_column_to_shippings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddExtraColumnToShippingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shippings', function (Blueprint $table) {
+            $table->string('origin_longtitude')->nullable()->after('origin');
+            $table->string('origin_latitude')->nullable()->after('origin');
+            $table->string('destination_longtitude')->nullable()->after('destination');
+            $table->string('destination_latitude')->nullable()->after('destination');
+            $table->string('current_location')->nullable()->afer('destination');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('shippings', function (Blueprint $table) {
+            //
+        });
+    }
+}
