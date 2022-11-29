6ed208ab03986c560fd5bf65fce792d9f6c13ab4
unknown <jed.macarilay@gmail.com>
2022-11-29 10:11:17 +0800

    Added new user for seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,11 +14,37 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'Admin',
-            'email' => 'admin@denlee',
-            'user_type' => 'admin',
-            'password' => Hash::make('denlee@pass'),
-        ]);
+        DB::table('users')->insert(
+            [
+                'name' => 'Admin',
+                'email' => 'admin@denlee',
+                'user_type' => 'admin',
+                'password' => Hash::make('denlee@pass'),
+            ],
+            [
+                'name' => 'Edrian Agenga',
+                'email' => 'dev1@denlee',
+                'user_type' => 'admin',
+                'password' => Hash::make('denlee@pass'),
+            ],
+            [
+                'name' => 'Nico Candaroma',
+                'email' => 'dev2@denlee',
+                'user_type' => 'admin',
+                'password' => Hash::make('denlee@pass'),
+            ],
+            [
+                'name' => 'Vargas Kenneth',
+                'email' => 'dev3@denlee',
+                'user_type' => 'admin',
+                'password' => Hash::make('denlee@pass'),
+            ],
+            [
+                'name' => 'Kim Caparas',
+                'email' => 'dev4@denlee',
+                'user_type' => 'admin',
+                'password' => Hash::make('denlee@pass'),
+            ],
+        );
     }
 }
