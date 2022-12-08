6cda45af6ec6df6885ec2ae2deac1f7888534996
unknown <jed.macarilay@gmail.com>
2022-12-09 06:41:50 +0800

    Add temporary login design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,16 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="cold-md-8 col-lg-5">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
                 <div class="card-body">
+                    <h1 class="text-center mb-10">Denlee Variety Store</h1>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -41,7 +40,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-denlee">
                                     {{ __('Login') }}
                                 </button>
                             </div>
@@ -53,3 +52,10 @@
     </div>
 </div>
 @endsection
+
+<style>
+    .btn-denlee {
+        background-color: #49D9A0 !important;
+        color: white !important; 
+    }
+</style>
